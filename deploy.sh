db1331a2a410895a4cd96669f6e9d269c2ea21a3
roye-b <rbega2@gmail.com>
2024-12-31 08:28:29 -0800

    try

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+sudo apt update && sudo apt upgrade -y
+
+cd ~/app || exit
+
+npm install
+npm run build
+
+#  (Systemd)
+sudo cp netflix-movie-catalog.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable netflix-movie-catalog.service
+sudo systemctl restart netflix-movie-catalog.service
 # TODO your deploy script implementation...
