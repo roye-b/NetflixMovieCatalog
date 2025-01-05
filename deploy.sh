d12bc1f249a78965ff2cc0d4e0a4482cb9b96e31
roye-b <rbega2@gmail.com>
2025-01-05 08:35:42 -0800

    cicd2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-sudo  systemctl daemon-reload
-sudo systemctl stop Netflix_instance_UP.service
-sudo systemctl start Netflix_instance_UP.service
-if [ -e /home/roye/Desktop/roye-key.pem ]; then
-  rm /home/roye/Desktop/roye-key.pem
-fi
-# TODO your deploy script implementation...
+docker-compose down
+docker-compose up -d
